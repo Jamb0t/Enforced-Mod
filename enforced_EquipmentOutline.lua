9be64dc9824eb99ec3450c0f2edf4d605c3d2274
Jamb0t <Jamster19@hotmail.com>
2016-03-19 18:48:54 -0700

    Delete enforced_EquipmentOutline.lua

--- enforced_EquipmentOutline.lua
+++
@@ -1,5 +0,0 @@
-
-local equipmentOutlineModelLookup = GetUpValue( EquipmentOutline_UpdateModel, "lookup" )
-if equipmentOutlineModelLookup then
-    table.insert(equipmentOutlineModelLookup, "HeavyMachineGun")
-end
