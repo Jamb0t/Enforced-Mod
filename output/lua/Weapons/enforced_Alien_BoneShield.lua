3793944ef1232daee4df55b702b33206920af624
theqizmo <theqizmo@gmail.com>
2016-05-28 16:19:48 -0700

    Boneshield fix
    Boneshield no longer uses "double energy" when activating

--- output/lua/Weapons/enforced_Alien_BoneShield.lua
+++ output/lua/Weapons/enforced_Alien_BoneShield.lua
@@ -4,28 +4,35 @@ function (self)
     return self.timeLastBoneShield + kBoneShieldCooldown > Shared.GetTime()
 end)
 
+local orig_BoneShield_OnPrimaryAttack
+orig_BoneShield_OnPrimaryAttack = Class_ReplaceMethod( "BoneShield", "OnPrimaryAttack",
+function (self, player)
+    if not self.primaryAttacking then
+        if self:GetEnergyCost() < player:GetEnergy() and player:GetIsOnGround() and self:GetCanUseBoneShield(player) then
+            player:DeductAbilityEnergy(self:GetEnergyCost())
+            self.primaryAttacking = true
+            self.timeLastBoneShield = Shared.GetTime()
+        end
+    end
+end)
+
+
 local orig_BoneShield_OnPrimaryAttackEnd
 orig_BoneShield_OnPrimaryAttackEnd = Class_ReplaceMethod( "BoneShield", "OnPrimaryAttackEnd",
 function (self, player)
-    if self.primaryAttacking then
+    if self.primaryAttacking then 
+        -- Start cooldown from this point
+        self.timeLastBoneShield = Shared.GetTime() - kBoneShieldMaxDuration
         self.primaryAttacking = false
-    end   
+    end    
 end)
 
 local orig_BoneShield_OnProcessMove
 orig_BoneShield_OnProcessMove = Class_ReplaceMethod( "BoneShield", "OnProcessMove",
 function (self, input)
     if self.primaryAttacking then
-
         local player = self:GetParent()
         if player then
-
-            if self.firstboneshieldframe then
-                player:DeductAbilityEnergy(self:GetEnergyCost())
-                self.firstboneshieldframe = false
-            end
-
-            local energy = player:GetEnergy()
             player:DeductAbilityEnergy(input.time * kBoneShieldEnergyPerSecond)
 
             if player:GetEnergy() == 0 then
@@ -33,9 +40,5 @@ function (self, input)
                 self.timeLastBoneShield = Shared.GetTime()
             end
         end
-    else
-
-        self.firstboneshieldframe = true
-
     end
 end)
