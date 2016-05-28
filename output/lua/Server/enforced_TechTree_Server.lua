c40c86a06b7073b9cbb9a5fd2201d63e2a55cc54
theqizmo <theqizmo@gmail.com>
2016-05-27 22:20:21 -0700

    Fix Dual Exo issue
    Research was missing.

--- output/lua/Server/enforced_TechTree_Server.lua
+++ output/lua/Server/enforced_TechTree_Server.lua
@@ -25,8 +25,8 @@ function (self, techId, prereq1, prereq2, addOnTechId)
 		self:AddTargetedActivation(kTechId.DropExosuit, kTechId.ExosuitTech,     kTechId.None)
 		self:AddBuyNode(kTechId.Exosuit,                kTechId.ExosuitTech,     kTechId.None)
 		self:AddBuyNode(kTechId.ClawRailgunExosuit,     kTechId.ExosuitTech,     kTechId.None)
-		self:AddBuyNode(kTechId.UpgradeToDualMinigun,   kTechId.DualMinigunTech, kTechId.None)
-		self:AddBuyNode(kTechId.UpgradeToDualRailgun,   kTechId.DualMinigunTech, kTechId.None)
+		self:AddBuyNode(kTechId.UpgradeToDualMinigun,   kTechId.TwoCommandStations, kTechId.None)
+		self:AddBuyNode(kTechId.UpgradeToDualRailgun,   kTechId.TwoCommandStations, kTechId.None)
 		self:AddTargetedBuyNode(kTechId.NapalmGrenade,  kTechId.GrenadeTech)
 	-- Original
 	else
@@ -46,10 +46,10 @@ function (self, techId, prereq1, prereq2)
 		orig_TechTree_AddTargetedActivation(self, techId, kTechId.TwoCommandStations, prereq2)
     elseif techId == kTechId.DropHeavyMachineGun then
         -- Add HMG
-        orig_TechTree_AddTargetedActivation(self, kTechId.DropHeavyMachineGun, kTechId.AdvancedArmory, prereq2)				
+        orig_TechTree_AddTargetedActivation(self, kTechId.DropHeavyMachineGun, kTechId.AdvancedArmory, prereq2)
 	else
 		-- Original
-		orig_TechTree_AddTargetedActivation(self, techId, prereq1, prereq2)	
+		orig_TechTree_AddTargetedActivation(self, techId, prereq1, prereq2)
 	end
 end)
 
@@ -59,18 +59,18 @@ function (self, techId, prereq1, prereq2, addOnTechId)
 
 	if techId == kTechId.Crush then
     -- personal upgrades (all alien types)
-		--self:AddBuyNode(kTechId.Crush, kTechId.Shell, kTechId.None, kTechId.AllAliens)    
-		orig_TechTree_AddBuyNode(self, kTechId.Carapace, kTechId.Shell, kTechId.None, kTechId.AllAliens)    
+		--self:AddBuyNode(kTechId.Crush, kTechId.Shell, kTechId.None, kTechId.AllAliens)
+		orig_TechTree_AddBuyNode(self, kTechId.Carapace, kTechId.Shell, kTechId.None, kTechId.AllAliens)
 		orig_TechTree_AddBuyNode(self, kTechId.Regeneration, kTechId.Shell, kTechId.None, kTechId.AllAliens)
-		
+
 		--self:AddBuyNode(kTechId.Focus, kTechId.Veil, kTechId.None, kTechId.AllAliens)
 		orig_TechTree_AddBuyNode(self, kTechId.Aura, kTechId.Veil, kTechId.None, kTechId.AllAliens)
 		orig_TechTree_AddBuyNode(self, kTechId.Phantom, kTechId.Veil, kTechId.None, kTechId.AllAliens)
-		
-		--self:AddBuyNode(kTechId.Silence, kTechId.Spur, kTechId.None, kTechId.AllAliens)  
-		orig_TechTree_AddBuyNode(self, kTechId.Celerity, kTechId.Spur, kTechId.None, kTechId.AllAliens)  
+
+		--self:AddBuyNode(kTechId.Silence, kTechId.Spur, kTechId.None, kTechId.AllAliens)
+		orig_TechTree_AddBuyNode(self, kTechId.Celerity, kTechId.Spur, kTechId.None, kTechId.AllAliens)
 		orig_TechTree_AddBuyNode(self, kTechId.Adrenaline, kTechId.Spur, kTechId.None, kTechId.AllAliens)
-	elseif techId == kTechId.Carapace or techId == kTechId.Regeneration or 
+	elseif techId == kTechId.Carapace or techId == kTechId.Regeneration or
 	       techId == kTechId.Aura or techId == kTechId.Vampirism or techId == kTechId.Focus or
 	       techId == kTechId.Silence or techId == kTechId.Celerity or techId == kTechId.Adrenaline then
 	    -- do nothing
